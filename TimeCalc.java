9dba16291e33eb7b99be971080b422b66d869704
Olivia Diamond <oliviadiamond@Olivias-MacBook-Air-2.local>
2024-11-15 15:29:27 +0200

    first version of TimeCalc

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,47 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        String time = args[0];
+        int minutesToAdd = Integer.parseInt(args[1]);
+
+        String[] timeParts = time.split(":");
+    
+    int hoursOriginal = Integer.parseInt(timeParts[0]);
+    int minutesOriginal = Integer.parseInt(timeParts[1]);
+
+   //System.out.println( "Hours: " + hoursOriginal );
+    //System.out.println( "minutes: " + minutesOriginal);
+    //System.out.println( "Minutes to add: " + minutesToAdd);
+
+    int totalMinutes = (hoursOriginal * 60) + minutesOriginal + minutesToAdd;
+    int totalHours = totalMinutes/60;
+    int newHours = totalHours % 24;
+    int newMinutes = totalMinutes - (totalHours * 60);
+
+   // System.out.println( "new minutes: " + newMinutes);
+   // System.out.println( "new hours: " + newHours);
+
+   String strNewMinutes = String.valueOf(newMinutes);
+        strNewMinutes = "0" + strNewMinutes;
+
+    String strNewHours = String.valueOf(newHours);
+    
+        if (newHours<10){
+            strNewHours = "0" + strNewHours;
+    }
+   
+   
+    if (newMinutes<10){
+        System.out.println(strNewHours + ":" +strNewMinutes );
+    }
+
+   if (newMinutes>10){
+        System.out.println( strNewHours + ":" + newMinutes);
+   }
+   
+   
+
+
+
+
     }
 }
