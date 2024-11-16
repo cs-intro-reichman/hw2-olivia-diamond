ff2405afe65b4c6dd9ca12762da8aab68b8ad2ac
Olivia Diamond <oliviadiamond@Olivias-MacBook-Air-2.local>
2024-11-16 14:11:03 +0200

    first version

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,33 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+
+String word = args[0];
+int times = Integer.parseInt(args[1]);
+
+String wordUpper = word.toUpperCase();
+int length = word.length();
+
+String article = "a";
+
+String specialLetters = "AEFHILMNORSX";
+
+for (int x = 0; x< length ; x++){
+                char currentChar = wordUpper.charAt(x);
+        if (specialLetters.indexOf(currentChar) != -1){
+                System.out.println("Give me an " + currentChar + ":" + " " + currentChar + "!");
+
+        }else{
+                System.out.println("Give me a " + currentChar + ":" + " " + currentChar + "!");  
         }
+        
+        }
+
+System.out.println("What does that spell?");
+
+for (int i=0 ; i<times ; i++){
+        System.out.println(wordUpper + "!!!");
 }
+
+        }
+                }
