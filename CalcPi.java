1d590145e64492ce18aec6be4eddf33555853442
Olivia Diamond <oliviadiamond@Olivias-MacBook-Air-2.local>
2024-11-20 22:28:08 +0200

    second version

--- CalcPi.java
+++ CalcPi.java
@@ -1,30 +1,24 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    int max = Integer.parseInt(args[0]);
+	    int n = Integer.parseInt(args[0]);
 
+double c = 0.0;
 int i = 0;
-double piApprox1 = 0;
-double piApprox2 = 0;
+
+for(i = 0 ; i< n; i++){
+	if(i%2==0){
+c = c + (1.0)/((2*i)+1);
+	}else{
+		c = c - (1.0)/((2*i)+1);
+	}
+}
+
+c = 4*c;
 double pi = Math.PI;
 
-while (i<max){
-	double sum = (1.0/(1+(2*i)));
-	piApprox1 = piApprox1 + sum;
-	i=i+2;
-}
 
-i= 1;
-while (i<max){
-	double sum = (1.0/(1+(2*i)));
-	piApprox2 = piApprox2 - sum;
-	i=i+2;
-}
-
-double piApproxTotal = (double)piApprox1 + piApprox2;
-
-System.out.println("pi according to Java: " + pi);
-System.out.println("pi, approximated:     " + (piApproxTotal*4.0));
-
+System.out.println(c);
+System.out.println(pi);
 	}
 }
